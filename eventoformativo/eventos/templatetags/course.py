08f054f8de7d6ca4fa89e0ac8b5797cd051887f3
ErikFantomex <a217213318@unison.mx>
2022-12-02 02:10:14 -0700

    Cambio ala sorda

--- eventoformativo/eventos/templatetags/course.py
+++ eventoformativo/eventos/templatetags/course.py
@@ -10,3 +10,12 @@ def model_name(obj):
         return obj._meta.model_name
     except AttributeError:
         return None
+
+# segmentacion de imagenes descripcion
+# https://www.youtube.com/watch?v=1Jh4cLgF4o4
+# https://www.youtube.com/watch?v=XlS2QX9Z1vM
+# https://www.youtube.com/watch?v=6D0m6p8PmKU
+# https://www.youtube.com/watch?v=0oHvN7zTmBw
+# https://www.youtube.com/watch?v=7V9YqG3q8m8
+# https://www.youtube.com/watch?v=2Qc8VWdM0vI
+# https://www.youtube.com/watch?v=6D0m6p8PmKU
